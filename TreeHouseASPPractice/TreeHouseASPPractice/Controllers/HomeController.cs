c3df0293fe2bfa7d43a20e3e7f7cf01aeddf4512
Brent Able <Able.Brent@richlandcountysc.gov>
2025-06-13 16:01:04 -0400

    Added in Design Files

--- TreeHouseASPPractice/TreeHouseASPPractice/Controllers/HomeController.cs
+++ TreeHouseASPPractice/TreeHouseASPPractice/Controllers/HomeController.cs
@@ -20,6 +20,18 @@ namespace TreeHouseASPPractice.Controllers
             //}
 
             //return Content("Hello From the tree house practice");
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
 
